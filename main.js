0d6026a0d8bccedcf23022e21976c1a8ae59c97f
javierbarbozam <barboza.javier497@gmail.com>
2022-12-09 12:04:44 -0600

    add all exercises

--- main.js
+++ main.js
@@ -1,52 +1,74 @@
 function main() {
-  let array = [1, 2, 3, 4];
+  let firstArray = [3, "hola", "l", 2];
+  let secondArray = ["hola", "k", 4, 2];
 
-  function ejercicioUno() {
-    let triangulo = "***********";
-    let nuevoTriangulo = "";
+  function ejercicioUno(number) {
+    let matriz = [];
+    let izquierda = 0;
+    let derecha = number - 1;
 
-    for (i = 10; i >= 1; i--) {
-      nuevoTriangulo = triangulo.substring(0, i);
-      while (nuevoTriangulo.length < 10) {
-        nuevoTriangulo += "#";
-      }
-      console.log(nuevoTriangulo);
-    }
-  }
-
-  function ejercicioDos(a, b) {
-    let result = a;
-    let counter = 1;
-
-    do {
-      result *= a;
-      counter++;
-    } while (counter < b);
-
-    console.log(result);
-  }
-
-  function ejercicioTres(a, b) {
-    let resultado = a;
-    if (b === 0) {
-      return 1;
+    if (number <= 1) {
+      console.log(
+        "El parámetro es muy pequeño para realizar el ejercicio, ingrese un número mayor."
+      );
     } else {
-      return resultado * ejercicioTres(a, b - 1);
+      for (i = 0; i < number; i++) {
+        let array = [];
+        matriz.push(array);
+        for (j = 0; j < number; j++) {
+          array[j] = "";
+          array[izquierda] = "X";
+          array[derecha] = "X";
+        }
+        izquierda++;
+        derecha--;
+      }
+      console.log(matriz);
     }
   }
 
-  function ejercicioCuatro(array, sumatoria) {
-    if (array.length === 0) {
-      console.log(sumatoria);
-      return sumatoria;
+  // Ejercicio hecho con Heylin Loaiza
+  function ejercicioDos(a, b) {
+    a.sort();
+    b.sort();
+    let repetidos = [];
+
+    for (i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        repetidos.push(a[i], b[i]);
+      }
     }
-    ejercicioCuatro(array.slice(1, array.length), sumatoria + array[0]);
+
+    if (repetidos.length < 1) {
+      console.log("Los dos arreglos contienen los mismos elementos");
+    } else {
+      console.log(
+        "Los dos arreglos no contienen los mismos elementos.",
+        repetidos
+      );
+    }
   }
 
-  ejercicioUno();
-  ejercicioDos(2, 3);
-  console.log(ejercicioTres(2, 3));
-  ejercicioCuatro(array, 0);
+  function ejercicioTres() {
+    let n1 = parseInt(document.getElementById("n1").value);
+    let n2 = parseInt(document.getElementById("n2").value);
+    let operation = document.getElementById("operators").value;
+
+    if (operation === "add") {
+      document.getElementById("result").value = n1 + n2;
+    } else if (operation === "substract") {
+      document.getElementById("result").value = n1 - n2;
+    } else if (operation === "divide") {
+      document.getElementById("result").value = n1 / n2;
+    } else if (operation === "multiply") {
+      document.getElementById("result").value = n1 * n2;
+    }
+  }
+  let execute = document.getElementById("execute");
+  execute.addEventListener("click", ejercicioTres);
+
+  ejercicioUno(6);
+  ejercicioDos(firstArray, secondArray);
 }
 
 main();
